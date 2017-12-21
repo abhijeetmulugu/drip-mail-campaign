f38f6b606360e3c374970e6f5df22cc20383dc4a
Abhijeet <abhijeetmulugu27@gmail.com>
2017-12-21 22:12:31 +0530

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -6,8 +6,8 @@ from campaignManager import SendMessage
 
 from campaignManager import getmail
 
-schedule.every().day.at("19:26").do(SendMessage)
-schedule.every().day.at("19:28").do(getmail)
+schedule.every().day.at("12:00").do(SendMessage)
+schedule.every().day.at("11:30").do(getmail)
 while 1:
     schedule.run_pending()
     time.sleep(1)
